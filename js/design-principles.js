85691e555e8a2b1d13117a4c35f932af0daaeec3
gib-student <gib19021@byui.edu>
2022-01-13 19:28:49 -0700

    update

---
+++ js/design-principles.js
@@ -0,0 +1,4 @@
+var year = new Date().getFullYear();
+var modified = document.lastModified;
+document.getElementById("year").textContent = year;
+document.getElementById("last-modified").textContent = modified;
