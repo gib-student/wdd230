d29051b24ef4967f68f68f5800ab7dd8e8d3faef
gib-student <gib19021@byui.edu>
2022-02-26 22:59:00 -0700

    Update

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,7 +1,7 @@
 let sayThankYou = () => {
     window.location.href = 
-    'https://gib-student.github.io/wdd230-Main/chamber/thankyou.html';
-}
+        'https://gib-student.github.io/wdd230-Main/chamber/thankyou.html';
+};
 
 document.querySelector('.join-submit-btn').addEventListener('click', 
-sayThankYou);
+    sayThankYou);
