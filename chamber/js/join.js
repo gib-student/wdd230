cdc18046ce3550b3595fb4b93efc79d96ef94d4b
gib-student <gib19021@byui.edu>
2022-02-26 22:49:52 -0700

    Update

---
+++ chamber/js/join.js
@@ -0,0 +1,7 @@
+let sayThankYou = () => {
+    window.location.href = 
+    'https://gib-student.github.io/wdd230-Main/chamber/thankyou.html';
+}
+
+document.querySelector('.join-submit-btn').addEventListener('click', 
+sayThankYou);
--- chamber/js/regular_expression.js
+++
@@ -1,6 +0,0 @@
-// DONT ALTER THIS!!! This is what will determine whether the input for the
-// title field is valid or not. If it is valid, then match will return a 
-// string, but if not, then it will return null.
-const re = /[a-z-\s]{7,}/gi;
-let match = myString.match(re);
-console.log(match);
