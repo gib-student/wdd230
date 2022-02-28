0145e3ae3e61c3ff2a1c849ad31f39abae9f7e01
gib-student <gib19021@byui.edu>
2022-02-28 14:03:25 -0700

    Update

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,3 +1,16 @@
+// Get current date and time for the form
+const date = new Date();
+const dateTime = {
+    "month": date.getMonth(), 
+    "date": date.getDate(), 
+    "year": date.getFullYear(),
+    "hours": date.getHours(),
+    "minutes": date.getMinutes(),
+    "seconds": date.getSeconds()
+};
+document.querySelector('.time-loaded').value = dateTime;
+
+// Open thankyou.html when user submits
 let sayThankYou = () => {
     window.open('https://gib-student.github.io/wdd230-Main/chamber/thankyou.html',
     '_blank');
