9f574c22102a5589976fde6635eddd23f5818b81
gib-student <gib19021@byui.edu>
2022-03-07 15:29:35 -0700

    Update

---
+++ chamber/js/directory/directory.js
@@ -0,0 +1,24 @@
+// // Display view style depending on the viewport
+// // const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+// const cardsBox  = document.querySelector('.bus-cards-box');
+// const listBox   = document.querySelector('.bus-list-box');
+
+// // if (vw >= 800 && vw < 1000) {
+// //     console.log("If statement is true");
+// //     cardsBox.style.display  = 'none';
+// //     listBox.style.display   = 'block';
+// // }
+
+// // Change view style according to button presses
+// const cardsBtn  = document.querySelector('.card-btn');
+// const listBtn   = document.querySelector('.list-btn');
+
+// cardsBtn.onclick = function() {
+//     cardsBox.style.display  = 'none';
+//     listBox.style.display   = 'block';
+// };
+
+// listBtn.onclick = function() {
+//     cardsBox.style.display  = 'block';
+//     listBox.style.display   = 'none';
+// };
--- chamber/js/directory/make-directory.js
+++ chamber/js/directory/make-directory.js
@@ -110,3 +110,17 @@ function makeList(busData) {
         ul.appendChild(div);
     }
 }
+// Show/hide cards and list according to buttons
+const cardsBox  = document.querySelector('.business-cards-box');
+const listBox   = document.querySelector('.business-list-box');
+
+function hideCards() {
+    cardsBox.style.display  = "none";
+    listBox.style.display   = "block";
+}
+
+function hideList() {
+    cardsBox.style.display  = "block";
+    listBox.style.display   = "none";
+}
+
