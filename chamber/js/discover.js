a87a8e52da742c55ea8feb0c57a22b51865d4068
gib-student <gib19021@byui.edu>
2022-03-15 13:02:11 -0600

    Update

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -1,16 +1,3 @@
-// Progressive loading of images
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
-// console.log("imagesToLoad: " + String(imagesToLoad))
-// const loadImages = (image) => {
-//     image.setAttribute('src', image.getAttribute('data-src'));
-//     image.onLoad = () => {
-//         image.removeAttribute('data-src');
-//     };
-// };
-// imagesToLoad.forEach((img) => {
-//     loadImages(img);
-// });
-
 // Use local storage to calculate days between last time accessed
 // Get date data
 let date = new Date()
