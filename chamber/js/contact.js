704c8074ac7d07ae660a515eb0bdfdfc93427fc6
gib-student <gib19021@byui.edu>
2022-03-16 14:07:26 -0600

    Update

--- chamber/js/contact.js
+++ chamber/js/contact.js
@@ -1,6 +1,45 @@
 // Create directors' contacts
+fetch('https://raw.githubusercontent.com/gib-student/wdd230-Main/main/chamber/data/data.json')
+    .then(response => response.json())
+    .then (data => {
+        makeCards(data);
+        makeList(data);
+    });
+
+const imgFilepath = business.image;
+        if (imgFilepath != '') {
+            const imageUrl = 'https://raw.githubusercontent.com/gib-student/wdd230-Main/main/chamber/images/directory/' + imgFilepath.toString();
+            (async () => {
+                const response = await fetch(imageUrl);
+                const imageBlob = await response.blob();
+                const reader = new FileReader();
+                reader.readAsDataURL(imageBlob);
+                reader.onloadend = () => {
+                  const base64data = reader.result;
+                  img.src = base64data;
+                };
+              })();
+        }
 
 // Image lazy loading
+const images = document.querySelectorAll('[data-src]'); 
+const options = { threshold: [.5] } 
+function preloadImage(img) { 
+    const source = img.getAttribute('data-src'); 
+    img.src = source; 
+} 
+const io = new IntersectionObserver( (entries, io) => {
+     entries.forEach(entry => {
+        if(!entry.isIntersecting){ 
+            return; 
+        } 
+        else { 
+            preloadImage(entry.target); //call a function send in the image that is currently intersecting 
+            io.unobserve(entry.target); 
+        } 
+    }); 
+}, options); 
+images.forEach(images => { io.observe(images); });
 
 // Date at the top
 const date = new Date();
